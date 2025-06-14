d82f26b8352c943a2748e7de1cf692fd1f0a1b58
Dan Kingo <danieldejen23@gmail.com>
2025-06-14 22:18:08 +0300

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (_req:Request, res:Response) => {
+  res.send('Hello, World!');
+}
+);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+);
+export default app;
