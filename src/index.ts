202e599a1e5af3870f44fccdf36bf75c060a2e38
Dan Kingo <danieldejen23@gmail.com>
2025-06-16 16:17:45 +0300

    add cors whitelist

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express, {Request, Response} from 'express';
 import morgan from "morgan"
 import debug from 'debug';
-const appDebug = debug('app:startup');
+import cors from 'cors'
+
 import connectDB from './configs/db.js';
 import accessLogStream from './middlewares/loggerMiddleware.js';
+import corsOptions from './middlewares/corsOptionsMiddleware.js';
+
+const appDebug = debug('app:startup');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +15,8 @@ connectDB()
 
 app.use(express.json())
 app.use(morgan("combined",{stream:accessLogStream}))
+app.use(cors(corsOptions))
+
 app.get('/', (_req:Request, res:Response) => {
   res.send('Hello, World!');
 }
---
+++ src/middlewares/corsOptionsMiddleware.ts
@@ -0,0 +1,23 @@
+import { CorsOptions } from "cors";
+
+const whitelist: string[] = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://www.google.com",
+];
+
+const corsOptions: CorsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true); // Allow request
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+export default corsOptions;
