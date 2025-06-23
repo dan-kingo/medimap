3d1c50b28761a09fd28c186ffaa7631d02eea6f2
Daniel Dejen <134540194+dan-kingo@users.noreply.github.com>
2025-06-23 15:07:15 +0300

    Fix User App Package Conflicts

--- user/metro.config.js
+++ user/metro.config.js
@@ -2,4 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Enable support for reanimated
+config.resolver.alias = {
+  ...config.resolver.alias,
+  '@': './src',
+};
+
 module.exports = config;
